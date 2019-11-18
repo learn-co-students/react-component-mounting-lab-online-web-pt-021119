155a9c7a2fa29a5cdbedfb9beaf365db225cd8a3
github username <github email address>
2019-11-17 19:42:39 -0500

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -9,7 +9,13 @@ class Timer extends Component {
 
   // add your code here
 
+componentDidMount() {
+  this.interval = setInterval(this.clockTick, 1000)
+}
 
+componentWillUnmount() {
+  clearInterval (this.interval);
+}
 
 
 
@@ -45,6 +51,7 @@ class Timer extends Component {
   handleClose = () => {
     this.props.removeTimer(this.props.id)
   }
+
 
 
 }
